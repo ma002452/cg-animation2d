c640ec9602f6b90f0743b425145f6acde62942b1
Seoyeon Ma <ma002452@stthomas.edu>
2024-03-07 23:37:37 -0600

    finished bouncing ball problem and still working on spinning polygons

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,4 +1,5 @@
 import * as CG from './transforms.js';
+import { Matrix } from './matrix.js';
 
 class Renderer {
     // canvas:              object ({id: __, width: __, height: __})
@@ -15,20 +16,51 @@ class Renderer {
         this.start_time = null;
         this.prev_time = null;
 
+        // slide0
+        let centerX = 300;
+        let centerY = 300;
+        let radius = 60;
+        let num_edges = 100;
+        let Vx_0 = 100;
+        let Vy_0 = 100;
+
+        let vertices = [];
+        for (let angle=0; angle<2*Math.PI; angle+=2*Math.PI/num_edges) {
+            let x = Math.round(centerX + radius * Math.cos(angle));
+            let y = Math.round(centerY + radius * Math.sin(angle));
+            vertices.push(CG.Vector3(x, y, 1));
+        }
+        //
+
+        // slide1
+        let Vx_1_0 = 100;
+        let Vy_1_0 = 100;
+        //
+
         this.models = {
             slide0: [
-                // example model (diamond) -> should be replaced with actual model
                 {
-                    vertices: [
-                        CG.Vector3(400, 150, 1),
-                        CG.Vector3(500, 300, 1),
-                        CG.Vector3(400, 450, 1),
-                        CG.Vector3(300, 300, 1)
-                    ],
-                    transform: null
+                    vertices: vertices,
+                    center: CG.Vector3(centerX, centerY, 1),
+                    velocity: [Vx_0, Vy_0, 1],
+                    radius: radius,
+                    transform: new Matrix(3, 3)
                 }
             ],
-            slide1: [],
+            slide1: [
+                {
+                    vertices: [
+                        CG.Vector3(300, 200, 1),
+                        CG.Vector3(200, 300, 1),
+                        CG.Vector3(100, 200, 1),
+                        CG.Vector3(200, 100, 1)
+                    ],
+                    center: CG.Vector3(200, 200, 1),
+                    velocity: [Vx_1_0, Vy_1_0, 1],
+                    theta: Math.PI/4,
+                    transform: new Matrix(3, 3)
+                }
+            ],
             slide2: [],
             slide3: []
         };
@@ -86,6 +118,27 @@ class Renderer {
     //
     updateTransforms(time, delta_time) {
         // TODO: update any transformations needed for animation
+        // time sine last frame: P = previousP + velocity * delta_time
+        // note: time is in milliseconds -> time/1000 makes time in seconds
+
+        // slide0
+        let tx_0 = this.models.slide0[0].transform.values[0][2] + this.models.slide0[0].velocity[0] * delta_time/1000;
+        let ty_0 = this.models.slide0[0].transform.values[1][2] + this.models.slide0[0].velocity[1] * delta_time/1000;
+        CG.mat3x3Translate(this.models.slide0[0].transform, tx_0, ty_0);
+        console.log(this.models.slide0[0].transform);
+
+        // slide1 (translate * rotate * translate)
+        let tx_1_0 = this.models.slide1[0].transform.values[0][2] + this.models.slide1[0].velocity[0] * delta_time/1000;
+        let ty_1_0 = this.models.slide1[0].transform.values[1][2] + this.models.slide1[0].velocity[1] * delta_time/1000;
+        CG.mat3x3Translate(this.models.slide1[0].transform, tx_1_0, ty_1_0);
+        let translate1 = this.models.slide1[0].transform
+        
+        CG.mat3x3Rotate(this.models.slide1[0].transform, this.models.slide1[0].theta);
+        let rotate = this.models.slide1[0].transform;
+
+
+
+        console.log(this.models.slide1[0].transform);
     }
     
     //
@@ -111,12 +164,39 @@ class Renderer {
     //
     drawSlide0() {
         // TODO: draw bouncing ball (circle that changes direction whenever it hits an edge)
+
+        // set new vertices that form the ball as it gets translated
+        let newVertices = [];
+        for (let i=0; i<100; i++) {
+            let v = Matrix.multiply([this.models.slide0[0].transform, this.models.slide0[0].vertices[i]]);
+            newVertices.push(v);
+        }
+
+        // set new center of the ball as it gets translated
+        let newCenter = Matrix.multiply([this.models.slide0[0].transform, this.models.slide0[0].center]);
         
-        
+        // did the ball hit a right/left edge?
+        if (newCenter.values[0][0] + this.models.slide0[0].radius >= this.canvas.width) {
+            this.models.slide0[0].velocity[0] = -100;
+        }
+        if (newCenter.values[0][0] - this.models.slide0[0].radius <= 0) {
+            this.models.slide0[0].velocity[0] = 100;
+        }
+        // did the ball hit a bottom/top edge?
+        if (newCenter.values[1][0] + this.models.slide0[0].radius >= this.canvas.height) {
+            this.models.slide0[0].velocity[1] = -100;
+        }
+        if (newCenter.values[1][0] - this.models.slide0[0].radius <= 0) {
+            this.models.slide0[0].velocity[1] = 100;
+        }
+
+        let color = [66, 158, 245, 255];
+        this.drawConvexPolygon(newVertices, color);
+
         // Following lines are example of drawing a single polygon
         // (this should be removed/edited after you implement the slide)
-        let teal = [0, 128, 128, 255];
-        this.drawConvexPolygon(this.models.slide0[0].vertices, teal);
+        //let teal = [0, 128, 128, 255];
+        //this.drawConvexPolygon(newVertices, teal);
     }
 
     //
@@ -124,6 +204,24 @@ class Renderer {
         // TODO: draw at least 3 polygons that spin about their own centers
         //   - have each polygon spin at a different speed / direction
         
+        // translate to the origin
+        let newVertices0 = [
+            CG.Vector3(100, 0, 1),
+            CG.Vector3(0, 100, 1),
+            CG.Vector3(-100, 0, 1),
+            CG.Vector3(0, -100, 1)
+        ];
+        // rotate about the origin
+
+        
+        // translate back to the original location
+        
+
+
+
+
+        let color = [66, 158, 245, 255];
+        this.drawConvexPolygon(newVertices0, color);
         
     }
 
